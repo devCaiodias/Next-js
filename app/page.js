782a43dcb049573e278b59362348362767722516
devCaiodias <caiodiasmartins12@gmail.com>
2024-11-19 13:01:47 -0300

    Navegação entre páginas (Componente Link)

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-        <h1 className="bg-red-600">Hello Word!</h1>
+        <div>
+          <h1 className="bg-red-600">Hello Word!</h1>
+          <ul>
+            <li>
+              <Link href="/produtos"> Produtos </Link>
+            </li>
+          </ul>
+        </div>
   );
 }
--- app/produtos/page.js
+++ app/produtos/page.js
@@ -1,5 +1,22 @@
+import Link from "next/link"
+
 export default function Produtos() {
     return (
-        <h1>Pagina produto</h1>
+        <div>
+            <h1>Pagina produto</h1>
+            <ul>
+
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/produtos/pants/bluepants">bluePants</Link>
+                </li>
+                <li>
+                    <Link href="/produtos/pants/redpants">RedPants</Link>
+                </li>
+            </ul>
+            
+        </div>
     )
 }
