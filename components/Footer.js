adbfff17e084e54d97c8e169dd82b6c3b7efb168
devCaiodias <caiodiasmartins12@gmail.com>
2024-11-19 17:47:54 -0300

    Adicionando CSS no Next.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer(params) {
     return (
-        <h1 className="bg-green-500">
-             Footer
-        </h1>
+        <div className="h-8 text-center">
+            <h1 className="bg-green-500">
+                 Footer
+            </h1>
+        </div>
     )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,14 +2,13 @@ import Link from "next/link"
 
 export default function Navbar() {
     return(
-        <div>
-            <ul className="bg-blue-600">
+        <div className="block">
+            <ul className="bg-blue-600 px-7 py-7">
             <li>
-              <Link href="/produtos"> Produtos </Link>
+              <Link className="px-4" href="/produtos"> Produtos </Link>
+              <Link className="px-4" href="/"> Home </Link>
             </li>
-            <li>
-              <Link href="/"> Home </Link>
-            </li>
+
           </ul>
         </div>
     )
